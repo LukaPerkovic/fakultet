84cf0cc2a229528e8445917402b8798d4fade123
LukaPerkovic <luka.m.perkovic@gmail.com>
2025-02-19 16:08:05 +0100

    Changing frontend layour and input type

--- ml_mikroservisi/frontend/app.py
+++ ml_mikroservisi/frontend/app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import requests
 import os
-import tempfile
 import logging
-
+from PIL import Image
+from io import BytesIO
 from dotenv import load_dotenv, find_dotenv
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 try:
     ENV = os.environ["ENVIRONMENT"]
@@ -18,7 +16,7 @@ except KeyError:
     load_dotenv(find_dotenv())
     ENV = os.getenv("ENVIRONMENT", "DEV")
 
-logger.info(f"Current environemnt set as: {ENV}")
+logger.info(f"Current environment set as: {ENV}")
 
 host = "backend-service" if ENV == "PROD" else "localhost"
 port = 7860
@@ -26,31 +24,39 @@ endpoint = "transform"
 
 backend_url = f"http://{host}:{port}/{endpoint}"
 
-st.title("Image Upload and Prediction")
+st.title("Aplikacija: Titlovanje slike")
+st.write("**Ime:** Luka Perkovic")
+st.write("**Broj indeksa:** 2023410428")
+st.write("\n\n")
+st.write("\n\n")
 
-uploaded_file = st.file_uploader(
-    "Choose an image...", type=["jpg", "jpeg", "png"], key="unique_file_uploader"
-)
 
-if uploaded_file is not None:
-    # Save the uploaded file to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
-        temp_file.write(uploaded_file.getvalue())
-        temp_file_path = temp_file.name
+image_url = st.text_input("Unesi URL slike (http ili https)")
 
-    st.image(temp_file_path, caption="Uploaded Image", use_container_width=True)
-    st.write("")
-    st.write("Classifying...")
+if image_url:
+    try:
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image = Image.open(BytesIO(response.content))
 
-    # Send the temporary file path to the backend service
-    response = requests.post(backend_url, json={"file_path": temp_file_path})
+        col1, col2 = st.columns(2)
 
-    if response.status_code == 200:
-        result = response.json()
-        st.write("Prediction Results:")
-        st.write(result[0].get("generated_text"))
-    else:
-        st.write("Error: Unable to get prediction results")
+        with col1:
+            st.image(image, caption="Loaded Image", use_container_width=True)
 
-    # Clean up the temporary file
-    os.remove(temp_file_path)
+        st.write("")
+        st.write("Obrada...")
+
+        # Send the image URL to the backend service
+        response = requests.post(backend_url, json={"image_url": image_url})
+
+        if response.status_code == 200:
+            result = response.json()
+            with col2:
+                st.write("Prediction Results:")
+                st.write(result[0].get("generated_text"))
+        else:
+            st.write("Error: Unable to get prediction results")
+
+    except requests.exceptions.RequestException as e:
+        st.write(f"Unable to fetch image from URL.")
